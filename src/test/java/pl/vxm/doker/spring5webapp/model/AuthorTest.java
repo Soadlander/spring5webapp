54cf85c2e0e7201e67a80975f9e3d4e3053b792f
Marcin Kloskowski <kloskowski@gmail.com>
2018-10-03 15:15:24 +0200

    initial commit

---
+++ src/test/java/pl/vxm/doker/spring5webapp/model/AuthorTest.java
@@ -0,0 +1,10 @@
+package pl.vxm.doker.spring5webapp.model;
+
+import static org.testng.Assert.*;
+
+public class AuthorTest {
+
+    @org.testng.annotations.Test
+    public void testGetFirstName() {
+    }
+}
